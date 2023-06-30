99dd4610bb02befe51a77e86d52f57ad3792eed3
Rajeesh-Durai <127297740+Rajeesh-Durai@users.noreply.github.com>
2023-06-30 15:39:07 +0530

    Add files via upload

---
+++ BackEnd/Models/AppointmentDetails.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace BigBangProject.Models
+{
+    public class AppointmentDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [ForeignKey("DoctorDetails")]
+        public int DoctorId { get; set; }
+        [Required]
+        public string PatientName { get; set; }=string.Empty;
+        [Required]
+        public int PatientAge { get; set; }
+
+        [Required]
+        [Column(TypeName ="Date")]
+        public DateTime AppointmentDate { get; set; }
+
+        [Required]
+        public string? AppointmentTime { get; set; }
+    }
+}
---
+++ BackEnd/Models/DoctorDetails.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BigBangProject.Models
+{
+    public class DoctorDetails
+
+    {
+    [Key]
+    public int? Id { get; set; }
+
+    [Required]
+    public string DoctorName { get; set; } = string.Empty;
+
+    [Required]
+    public string Specialization { get; set; } = string.Empty;
+
+        [Required]
+        public int Experience { get; set; }
+    public ICollection<AppointmentDetails>? Appointments { get; set; }
+}
+}
